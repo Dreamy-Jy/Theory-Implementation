a88a4ed9004ae7c3bbf5ef4f53d90014d396e72f
Jordane Thomas <thomasjordane7@gmail.com>
2018-01-23 12:37:13 -0500

    SelectionSort Java Implementation

---
+++ Algorithms/Sorting/Selection Sort/README.md
@@ -0,0 +1,5 @@
+# Selection Sort
+
+The Selection Sort is Simple O(n<sup>2</sup>) sorting Algorithm.
+
+## How it Works
---
+++ Algorithms/Sorting/Selection Sort/SelectionSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class SelectionSort {
+  // sorts array into assenting order
+  public static void selectionSort(int[] array) {
+
+    for(int pointerIndex = 0; pointerIndex < array.length; pointerIndex++) {
+      int minValueIndex = pointerIndex;
+
+      for(int lookerIndex = pointerIndex + 1; lookerIndex < array.length; lookerIndex++)
+        if (array[lookerIndex] < array[minValueIndex]) {minValueIndex = lookerIndex;}
+
+      if(minValueIndex != pointerIndex) {swap(array, minValueIndex, pointerIndex);}
+    }
+  }
+
+  public static void swap(int[] array, int lowerValueIndex, int highValueIndex) {
+    int val = array[lowerValueIndex];
+    array[lowerValueIndex] = array[highValueIndex];
+    array[highValueIndex] = val;
+  }
+
+  public static void main(String[] args) {
+    int[] array = generateRandomIntArray(100);
+    selectionSort(array);
+  }
+/*helper methods for testing*/
+  public static int[] generateRandomIntArray(int size) {
+    int [] array = new int[(int) (Math.random()*size + 1)];
+
+    for (int i = 0; i < array.length; i++)
+      array[i] = (int) (Math.random()*(size*10));
+
+    return array;
+  }
+}
